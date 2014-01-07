80aab584ec567ab9509f61d1d125322a2615c154
Nadine <nadine.lamorte@cmore-automotive.com>
2014-01-07 21:57:20 +0100

    1st edit

---
+++ Ampelschaltung_1.ino
@@ -0,0 +1,28 @@
+#define DELAY1 10000  //Pause 1 für 10 sekunden
+#define DELAY2 2000  //Pause 2 für 2 Sekunden
+#define DELAY3 3000  //Pause 3 für 3 Sekunden
+
+int ledPinRot = 6;
+int ledPinGelb = 8;
+int ledPinGruen = 10;
+
+void setup() {
+  pinMode(ledPinRot, OUTPUT);
+  pinMode(ledPinGelb, OUTPUT);
+  pinMode(ledPinGruen, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPinRot, HIGH);
+  delay(DELAY1);
+  digitalWrite(ledPinGelb, HIGH);
+  delay(DELAY2);
+  digitalWrite(ledPinRot, LOW);
+  digitalWrite(ledPinGelb, LOW);
+  digitalWrite(ledPinGruen, HIGH);
+  delay(DELAY3);
+  digitalWrite(ledPinGruen, LOW);
+  digitalWrite(ledPinGelb, HIGH);
+  delay(DELAY3);
+  digitalWrite(ledPinGelb, LOW);
+}
